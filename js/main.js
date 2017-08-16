31a523119551ecedf88129e7c6f013928b2eb741
Thomas Wolfe <tewolfe2@gmail.com>
2017-08-15 21:24:04 -0700

    added site and buttons

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
-  
 
-  $('.nav').on('click','li',() => {
+
+  $('.nav').on('click', 'li', function() {
     $('.navbar-toggle').click();
   });
- console.log('hi2');
 
-    var colors = [
+  $(".card").flip();
+
+
+
+  var colors = [
     ['#6ed4c0', '#ffffff'],
     ['#6ed4c0', '#ffffff'],
     ['#6ed4c0', '#ffffff'],
@@ -15,12 +18,14 @@ $(document).ready(function() {
     ['#6ed4c0', '#ffffff']
   ];
 
+  var imgPath = 'images/logos'
   var color = ['#6ed4c0', '#ffffff'];
-
-  var percentages = [20,70,60,80,70,70,80];
-  for (var i = 1; i <= 7; i++) {
+  var logos = [imgPath+'/react.png', imgPath+'/redux.png',  imgPath+'/logo_JavaScript.png', imgPath+'/mongo.jpg', imgPath+'/angular.png',imgPath+'/html5.svg', imgPath+'/css3.svg',imgPath+'/nodejs.jpeg', imgPath+'/bootstrap.png', imgPath+'/expressjs.png', imgPath+'/socket.png', imgPath+'/mysql.png']
+  var percentages = [70, 60, 80, 80, 70, 70, 80, 70, 90, 80, 40, 50];
+  for (var i = 1; i <= 12; i++) {
     var child = document.getElementById('circles-' + i),
-      percentage = percentages[i-1];
+      percentage = percentages[i - 1];
+
 
     Circles.create({
       id: child.id,
@@ -28,13 +33,13 @@ $(document).ready(function() {
       radius: 80,
       width: 10,
       // number:    percentage / 1,
-      // text:       '%',
+      text: '<img class="skillLogo img-circle" src="' + logos[i - 1] + '"></img>',
       colors: color
     });
   }
 
 
 
-  
+
 
 });
